c06298b79344b2dc671c5ddc06d339508d3525ac
William <45665895+Williais@users.noreply.github.com>
2023-09-17 17:26:03 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# nlwia
+ Projeto finalizado em conjuto com a rocket para o estudo de integração de uma IA para a conversão de um short do youtube para texto resumido
---
+++ server/download.js
@@ -0,0 +1,26 @@
+import ytdl from 'ytdl-core'
+import fs from 'fs'
+
+export const download = (videoId) => new Promise((resolve, reject) => {
+
+    const videoURL = "https://www.youtube.com/shorts/" + videoId
+    console.log('realizando o download do video:', videoId)
+
+    ytdl(videoURL, {quality: "lowestaudio", filter: "audioonly"}).
+    on("info", (info) => {
+        const seconds = info.formats[0].approxDurationsMs / 1000
+        if(seconds > 60){
+            throw new Error("A duração desse vídeo é maior do que 60 segundos")
+        }
+    }
+    ).on("end", () => {
+        console.log("Download do video finalizado.")
+        resolve()
+
+    }
+    ).on("error", (error) => {
+        console.log("não foi possivel fazer o download. Detalhe do erro:", error)
+        reject(error)
+    })
+    .pipe(fs.createWriteStream("./tmp/audio.mp4"))
+})
